cd08cc97eb4548c9fee2a418c0651836beb8e376
Montesi-Yuri <130767097+Montesi-Yuri@users.noreply.github.com>
2023-07-24 13:52:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  }
+}
+
+</script>
+
+<template>
+
+  <HeaderComponent>
+  </HeaderComponent>
+
+  <MainComponent>
+  </MainComponent>
+
+  <FooterComponent>
+  </FooterComponent>
+
+</template>
+
+<style lang="scss">
+@use './assets/scss/main.scss';
+
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+  }
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/main.scss';
+
+</style>
